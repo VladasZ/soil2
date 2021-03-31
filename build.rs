2e1a47258148e58a5372e32aea3d527004e7b10d
Vladas Zakrevksis <146100@gmail.com>
2021-03-31 13:54:35 +0300

    build

--- build.rs
+++ build.rs
@@ -1,7 +1,22 @@
 extern crate cmake;
 extern crate pkg_config;
 
+use std::env;
+use cmake::Config;
+
 fn main() {
     let dst = cmake::build("soil2");
     println!("cargo:rustc-link-search=native={}", dst.display());
+
+
+    let mut config = Config::new("soil2");
+
+    let dst = config
+        .profile("Release")
+        .build();
+
+    let out_dir = env::var("OUT_DIR").unwrap();
+    println!("cargo:rustc-link-search=native={}/lib", dst.display());
+   // println!("cargo:rustc-link-lib=static=freetype");
+    println!("cargo:outdir={}", out_dir);
 }
