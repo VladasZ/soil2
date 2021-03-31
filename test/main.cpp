08e468e4fe694b37e25a8ecd39e38d6f04cb3752
Vladas Zakrevksis <146100@gmail.com>
2021-03-31 15:26:30 +0300

    fixed mac test buils

--- test/main.cpp
+++ test/main.cpp
@@ -1,8 +1,18 @@
 
+#include <iostream>
+
+using namespace std;
+
 #include "../SOIL2/SOIL2.h"
 
 int main() {
 
+    
+    
 	SOIL_free_image_data(nullptr);
 	
+    cout << "Hello" << endl;
+    
+    return 0;
+    
 }
