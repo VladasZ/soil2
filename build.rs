9ab373df37834a559d2feeb85228d30f981ff740
dev <dev@Vladass-MacBook-Pro-2.local>
2021-04-01 16:43:31 +0300

    sokol

--- build.rs
+++ build.rs
@@ -7,6 +7,10 @@ use cmake::Config;
 fn main() {
     let dst = cmake::build("soil2");
 
+    let out_dir = env::var("OUT_DIR").unwrap();
+    
     println!("cargo:rustc-link-search=native={}", dst.display());
     println!("cargo:rustc-link-lib=static=soil2");
+    println!("cargo:outdir={}", out_dir);
+
 }
