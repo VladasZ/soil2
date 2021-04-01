34ea1095e2ade46f1733676790e434de81d256a9
dev <dev@Vladass-MacBook-Pro-2.local>
2021-04-01 16:59:13 +0300

    sokol

--- build.rs
+++ build.rs
@@ -1,19 +1,7 @@
 extern crate cmake;
-extern crate pkg_config;
-
-use std::env;
-use cmake::Config;
 
 fn main() {
     let dst = cmake::build("soil2");
-
-    
-    /// println!("cargo:rustc-link-search=native={}", dst.display());
     println!("cargo:rustc-link-search=native={}", dst.display());
     println!("cargo:rustc-link-lib=static=soil2");
-
-
-    let out_dir = env::var("OUT_DIR").unwrap();
-    println!("cargo:outdir={}", out_dir);
-
 }
